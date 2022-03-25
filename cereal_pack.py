f30e4d21fdb528ce13726cd7abbadae00641152b
Crabmandable <zach.gerstman@gmail.com>
2022-03-25 16:16:54 +0300

    Created a CMakeLists
    Plus tidied up a bit

--- cereal_pack.py
+++ cereal_pack.py
@@ -3,6 +3,9 @@ from generator import *
 import sys
 import pathlib
 
+def path_of_class(root, schema):
+    return pathlib.Path(root, *schema.name_with_namespace.split('::')[:-1], schema.name + '.hpp')
+
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description='Generate cereal pack c++ classes from a list of schemas.')
 
@@ -12,13 +15,21 @@ if __name__ == '__main__':
     arg_parser.add_argument('--output', '-o', metavar='output_dir', dest='out_dir', required=True,
                         help='output directory to place generated classes')
 
+    arg_parser.add_argument('--cmake', '-c', dest='cmake', required=False, default=False, action='store_true',
+                        help='output the paths for the classes that will be generated, seperated with `;`')
+
     args = arg_parser.parse_args()
 
     schemas = parser.load_schemas(args.schemas)
+    if args.cmake:
+        outputs = list(map(lambda s: str(path_of_class(args.out_dir, s)), schemas.values()))
+        sys.stdout.write(';'.join(outputs))
+        sys.exit()
+
     pathlib.Path(args.out_dir).mkdir(exist_ok=True)
 
     for s in schemas.values():
-        schema_dir = pathlib.Path(args.out_dir, *s.name_with_namespace.split('::')[:-1])
+        schema_dir = path_of_class(args.out_dir, s).parent
         schema_dir.mkdir(parents=True, exist_ok=True)
         with open(schema_dir / (s.name + '.hpp'), 'w') as file:
             file.write(generate.header_file(s))
